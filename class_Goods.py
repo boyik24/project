e624130d91bcda0a8b1c0a637ae303348dcf3d5b
admin <admin@gmail.com>
2024-12-16 20:07:38 +0500

    class good

---
+++ class_Goods.py
@@ -0,0 +1,16 @@
+class Goods:
+    def __init__(self,name,cost,date):
+        self.name=name
+        self.cost=cost
+        self.date=date
+        self.count=100
+good1=Goods("saryog",50000,[10,12,2024])
+good2=Goods("gosht",95000,[15,10,2024])
+good3=Goods("tuxum",2000,[11,10,2024])
+good4=Goods("moy",30000,[15,10,2024])
+good5=Goods("pomidor",8000,[9,10,2024])
+good6=Goods("bodring",6000,[15,10,2024])
+good7=Goods("olma",15000,[7,10,2024])
+good8=Goods("suv",3000,[15,10,2024])
+good9=Goods("choy",1000,[15,10,2024])
+good10=Goods("shokolad",12000,[12,10,2024])
