76ec9d6b98d4e0d44cec9a1ed0a6db6beff53015
admin <admin@gmail.com>
2024-12-16 21:42:12 +0500

    t

--- team_project1.py
+++ team_project1.py
@@ -4,7 +4,10 @@ from class_Market import supermarket1
 
 
 def Shop():
-    # client=input()
+    client_phone=input("Kliyentning telefon raqami : ")
+    for client in users:
+        if int(client_phone)==client.phone:
+            new_client=client
     print("No  Mahsulot nomi  Narxi (so'm)  Muddati")
     for i in range(1,len(goods)):
         print(f"{i}".ljust(3),f"{goods[i].name}".ljust(14), f"{goods[i].cost}".ljust(13),f"{(goods[i]).date[0]}.{goods[i].date[1]}.{goods[i].date[2]}")
