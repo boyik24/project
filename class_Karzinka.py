0581e778c5f7d9a1125f413d3a077abf779762ce
Ziyo6ka <shamsiddinovziyovuddin2010@gmail.com>
2024-12-17 23:24:17 +0500

    projekt_names

---
+++ class_Karzinka.py
@@ -0,0 +1,12 @@
+class Karzinka:
+    def __init__(self):
+        self.name=[]
+        self.umumiy_narx=0
+        self.mahsulotlar=[]
+
+karzinka1=Karzinka()
+karzinka2=Karzinka()
+karzinka3=Karzinka()
+karzinka4=Karzinka()
+
+karzinkalar=[karzinka1,karzinka2,karzinka3,karzinka4]
